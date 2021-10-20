c93b64c0334a43c90453bfb1e426450d23ba2bab
David Fletcher <=>
2021-10-20 01:50:38 -0400

    ready for release

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# new-with-ratios
+# New With Ratios
+![hero image](./assets/hero.png)
+
 An Aseprite extension to create a new file that has ratio presets to make file creation easier.
+
+## How to Use New With Ratios
+
+1. Download this extension by visiting the releases page! (Double click the extension once downloaded to install)
+2. In Aseprite, select `File > New With Ratio...`
+3. The dialog will allow you to first specify your pixel art dimensions constrained to a ratio before moving onto the standard file creation flow!
+
+![demonstration gif](https://media.giphy.com/media/9XoYy2cl7aN9rDMVzG/giphy.gif)
+
+Currently, the ratios supported are:
+
+* 1:1
+* 4:7
+* 7:8
+* 16:9
+
+I'm open to adding more ratios to the script, so please let me know if you'd like to see another ratio in this dialog as well! It's minimal effort to add new ones, so don't be shy!
+
+## Credits
+
+This extension was commissioned by [@timesnewredux](https://twitter.com/timesnewredux) on Twitter.
+
+As an advocate of open-source software, feel free to suggest edits, or just fork this repository and make your own! The license on this software is open for commercial and private use. This extension will remain free forever; however, if you'd like to buy me a coffee, you can do so here: 
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/L3L766S5F)
---
+++ extension/new-with-ratios.lua
@@ -0,0 +1,267 @@
+-- MIT License
+
+-- Copyright (c) 2021 David Fletcher
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+local function mainWindow()
+    local dialog = Dialog("New File...")
+    local ratio_width = 1
+    local ratio_height = 1
+
+    local function adjust_for_ratio(num, divisor, dividend)
+        return math.floor(num * (dividend / divisor))
+    end
+
+    -----------------------------------
+    -- RATIOS
+    -----------------------------------
+    dialog:separator {
+        id="ratios",
+        text="Ratios"
+    }
+
+    dialog:label {
+        id="help1",
+        label="NOTE:",
+        text="In the following ratios, the first value is the"
+    }
+
+    dialog:newrow()
+
+    dialog:label {
+        id="help2",
+        text="width, and the second is the height ( W : H )"
+    }
+
+    dialog:button {
+        id="onetoone",
+        text="1 : 1",
+        enabled=false,
+        onclick=function()
+            ratio_width = 1
+            ratio_height = 1
+            local new_height = adjust_for_ratio(dialog.data.width, ratio_width, ratio_height)
+            dialog:modify {
+                id="height",
+                text=new_height
+            }
+
+            dialog:modify {
+                id="onetoone",
+                enabled=false
+            }
+
+            dialog:modify {
+                id="fourtoseven",
+                enabled=true
+            }
+
+            dialog:modify {
+                id="seventoeight",
+                enabled=true
+            }
+
+            dialog:modify {
+                id="sixteentonine",
+                enabled=true
+            }
+        end
+    }
+
+    dialog:button {
+        id="fourtoseven",
+        text="4 : 7",
+        onclick=function()
+            ratio_width = 4
+            ratio_height = 7
+            local new_height = adjust_for_ratio(dialog.data.width, ratio_width, ratio_height)
+            dialog:modify {
+                id="height",
+                text=new_height
+            }
+
+            dialog:modify {
+                id="onetoone",
+                enabled=true
+            }
+
+            dialog:modify {
+                id="fourtoseven",
+                enabled=false
+            }
+
+            dialog:modify {
+                id="seventoeight",
+                enabled=true
+            }
+
+            dialog:modify {
+                id="sixteentonine",
+                enabled=true
+            }
+        end
+    }
+
+    dialog:newrow()
+
+    dialog:button {
+        id="seventoeight",
+        text="7 : 8",
+        onclick=function()
+            ratio_width = 7
+            ratio_height = 8
+            local new_height = adjust_for_ratio(dialog.data.width, ratio_width, ratio_height)
+            dialog:modify {
+                id="height",
+                text=new_height
+            }
+
+            dialog:modify {
+                id="onetoone",
+                enabled=true
+            }
+
+            dialog:modify {
+                id="fourtoseven",
+                enabled=true
+            }
+
+            dialog:modify {
+                id="seventoeight",
+                enabled=false
+            }
+
+            dialog:modify {
+                id="sixteentonine",
+                enabled=true
+            }
+        end
+    }
+
+    dialog:button {
+        id="sixteentonine",
+        text="16 : 9",
+        onclick=function()
+            ratio_width = 16
+            ratio_height = 9
+            local new_height = adjust_for_ratio(dialog.data.width, ratio_width, ratio_height)
+            dialog:modify {
+                id="height",
+                text=new_height
+            }
+
+            dialog:modify {
+                id="onetoone",
+                enabled=true
+            }
+
+            dialog:modify {
+                id="fourtoseven",
+                enabled=true
+            }
+
+            dialog:modify {
+                id="seventoeight",
+                enabled=true
+            }
+
+            dialog:modify {
+                id="sixteentonine",
+                enabled=false
+            }
+        end
+    }
+
+    -----------------------------------
+    -- PIXELS
+    -----------------------------------
+    dialog:separator {
+        id="pixels",
+        text="Size"
+    }
+
+    dialog:number {
+        id="width",
+        label="Width:",
+        decimals=0,
+        onchange=function()
+            local new_height = adjust_for_ratio(dialog.data.width, ratio_width, ratio_height)
+            dialog:modify {
+                id="height",
+                text=new_height
+            }
+        end
+    }
+
+    dialog:number {
+        id="height",
+        label="Height:",
+        decimals=0,
+        onchange=function()
+            local new_width = adjust_for_ratio(dialog.data.height, ratio_height, ratio_width)
+            dialog:modify {
+                id="width",
+                text=new_width
+            }
+        end
+    }
+
+    -----------------------------------
+    -- FINALIZE
+    -----------------------------------
+
+    dialog:separator {
+        id="footer",
+        text="Finalize"
+    }
+
+    dialog:button {
+        id="create",
+        text="Create New",
+        onclick=function()
+            dialog:close()
+            app.command.NewFile {
+                ui=true,
+                width=dialog.data.width,
+                height=dialog.data.height,
+                colorMode=ColorMode.RGB,
+                fromClipboard=false
+            }
+        end
+    }
+
+    -----------------------------------
+    -- ENSURE DEFAULT CANVAS SIZE
+    -----------------------------------
+
+    dialog:modify {
+        id="width",
+        text=64
+    }
+
+    dialog:modify {
+        id="height",
+        text=64
+    }
+
+    return dialog
+end
+
+mainWindow():show{ wait=true }
